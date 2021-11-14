4c7749a5273a847140567358be7e380a1dafac9e
Marebu <91844335+Marebu@users.noreply.github.com>
2021-11-14 16:48:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int n,i=1,b=1,a=0,c;
+   cin>>n;
+   
+   if(n>=2) {
+      cout<<b<<endl;
+      for(i=2;i<=n;i++) {
+      c=a+b;
+      cout<<c<<endl;
+      a=b;
+      b=c;
+   }
+      
+   } else{
+      cout<<"errore"<<endl;
+   }
    return 0;
 }
